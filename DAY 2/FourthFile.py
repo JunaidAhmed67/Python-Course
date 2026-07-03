e2834bc613a27bab0e81098e7e8f0758924d33c7
JunaidAhmed68 <jb7303721@gmail.com>
2026-07-03 22:18:03 +0500

    day 1

---
+++ DAY 2/FourthFile.py
@@ -0,0 +1,84 @@
+# data ={
+#     "name" : "junaid",
+#     "age" : 20,
+#     "course" : "python",
+#     "marks" : [23,89,34.3,53.6,78.78],
+#     "is_student" : True
+# }
+# data["name"]="ahmed"
+# data["collage"]="AIUB"
+# print(data)
+
+
+# student = {
+#     "name":"junaid",
+#     "age": 22,
+#     "score":{
+#         "phy":45,
+#         "chem":67,
+#         "maths":100
+#     }
+# }
+
+# # print(student["score"]["phy"])
+# # print(student["score"].keys())
+# # print(len(student))
+# # print(student.values())
+
+# # pairs =list(student.items())
+# # print(pairs[2])
+# # print(student.get("name2")) no error
+# # print(student["name2"]) error
+
+# print(student)
+# new_dic ={
+#     "city":"karachi",
+#     "country":"pakistan",
+#     "postCode":23627,
+# }
+
+# student.update(new_dic)
+# student.update({"createdAt":"newDate"})
+# student.update({"name":"aHMED"})
+# print(student)
+
+# sets = {1,2,3,4,4,"junaid","ahmed","junaid"} #store single value 
+# emptyset =set()#empty set
+
+# print(sets)#
+# print(type(emptyset))
+# emptyset.add("a")
+# emptyset.add("b")
+# emptyset.add("c")
+# emptyset.remove("a")
+# emptyset.clear()
+# print(emptyset)
+
+# set1 = {1,2,3}
+# set2 = {3,2,9}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+#p q
+# wordMeanings ={
+#     "table":["a piece of furniture","list of facts & figures"],
+#     "cat":"a small animal"
+# }
+# print(wordMeanings)
+
+# subjectList={"python","java","C++","python","javaScript","java","python","java","C++","C"
+#               }
+# length =len(subjectList)
+# print(length)
+
+# subMarks ={}
+# # chemMarks = )
+# mathMarks = float(input("enter marks of math: " ))
+# phyMarks = float(input("enter marks of phy: " ))
+# subMarks.update({"chem":float(input("enter marks of chem: " )),"maths":mathMarks,"phy":phyMarks})
+# # subMarks["maths":float(input("enter marks of chem: "))]
+# # subMarks["phy":float(input("enter marks of chem: "))]
+# print(subMarks)
+
+values={9,9.0}
+print(values)
---
+++ DAY 2/ThirdFile.py
@@ -0,0 +1,30 @@
+marks =[23.4,77.8,223.4,33.4,33.3]
+alpha = ["a","b","c","d"]
+# print(marks)
+# marks[0]="junaid"
+# print(len(marks))
+# print(marks[1:3])
+# print(marks[1:])
+# print(marks[2:len(marks)])
+# marks.append("junaid")
+# marks.sort()
+# marks.sort(reverse=True)
+# alpha.reverse()
+# marks.reverse()
+# alpha.remove("b")
+# alpha.pop(3)
+# print(alpha," ",marks)
+# tuples =(2,3,4,1,8)
+# print(tuples[1:])
+# movies = [input("one : "),input("two : "),input("Three : ")]
+# alpha = [1,2,3,2,1,7]
+# a = alpha.copy()
+# a.reverse()
+# if(alpha==a):
+#     print("its palendrome")
+# else:
+#     print("its not a palendrome")
+
+grades = ["Z","X","L","D","A","V","A","A","F","A"]
+grades.sort(reverse=True)
+print(grades)
