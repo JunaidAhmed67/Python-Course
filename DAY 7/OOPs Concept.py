a40c0ded5f42a5bd38a8a77844ca3a16941454df
JunaidAhmed68 <jb7303721@gmail.com>
2026-07-11 17:48:38 +0500

    day 7

--- DAY 7/OOPs Concept.py
+++ DAY 7/OOPs Concept.py
@@ -1,49 +1,49 @@
 # Abstraction
-# class Car:
-#     def __init__(self):
-#         self.acc = False
-#         self.brk = False
-#         self.clutch = False
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
 
-#     def start(self):
-#         self.clutch = True
-#         self.acc = True
-#         print("Car started...")
-#     def stop(self):
-#         self.clutch = False
-#         self.acc = False
-#         self.brk= True
-#         print("Car stoped...")
-
-
-# person = Car()
-# person.start()
-# person.stop()
+    def start(self):
+        self.clutch = True
+        self.acc = True
+        print("Car started...")
+    def stop(self):
+        self.clutch = False
+        self.acc = False
+        self.brk= True
+        print("Car stoped...")
 
 
+person = Car()
+person.start()
+person.stop()
 
-# class Account:
 
-#     def __init__(self,balance,accountNo):
-#         self.balance = balance
-#         self.accountNo = accountNo
 
-#     def debit(self, amount):
-#         if(self.balance>amount):
-#          self.balance -=amount
-#          print("Rs. ", amount , "was debited from your account.")
-#         else:
-#          print("you entered insuficient amount.")
-#         self.get_balance()
-#     def credit(self, amount):
-#         self.balance +=amount
-#         print("Rs. ", amount , "was credited from your account.")
-#         self.get_balance()
-#     def get_balance(self):
-#         print("Your (",self.accountNo,")current balance is : ",self.balance)
+class Account:
+
+    def __init__(self,balance,accountNo):
+        self.balance = balance
+        self.accountNo = accountNo
+
+    def debit(self, amount):
+        if(self.balance>amount):
+         self.balance -=amount
+         print("Rs. ", amount , "was debited from your account.")
+        else:
+         print("you entered insuficient amount.")
+        self.get_balance()
+    def credit(self, amount):
+        self.balance +=amount
+        print("Rs. ", amount , "was credited from your account.")
+        self.get_balance()
+    def get_balance(self):
+        print("Your (",self.accountNo,")current balance is : ",self.balance)
 
  
-# person1 = Account(1000,12345)
-# person1.get_balance()
-# person1.debit(500)
-# person1.credit(1000)
+person1 = Account(1000,12345)
+person1.get_balance()
+person1.debit(500)
+person1.credit(1000)
